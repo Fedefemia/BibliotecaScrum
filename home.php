03d601be5f16267aff0490ddd2915cca97293657
Server Linux Bot by FedeFemia <server-bot@fedefemia.com>
2025-11-27 22:47:51 +0000

    Aggiunto sistema di Routing con Whitelist

---
+++ home.php
@@ -0,0 +1,6 @@
+<h1>Benvenuto nella Home Protetta</h1>
+<p>Se vedi questo, il Router funziona!</p>
+<?php 
+// Testiamo se il DB è arrivato dal router
+if(isset($pdo)) echo "<b style='color:green'>Database Connesso dal Router!</b>"; 
+?>
