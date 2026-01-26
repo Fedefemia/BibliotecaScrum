f12b9c3b7f2302ed1dc21c82d44c46d8d3d4d46a
Server Linux Bot by FedeFemia <server-bot@fedefemia.com>
2026-01-26 02:00:03 +0000

    Auto-Backup DB: 2026-01-26 02:00

--- backup_db.sql
+++ backup_db.sql
@@ -174,7 +174,7 @@ CREATE TABLE `copie` (
   KEY `fk_copie_biblioteche` (`id_biblioteca`),
   CONSTRAINT `copie_ibfk_1` FOREIGN KEY (`isbn`) REFERENCES `libri` (`isbn`),
   CONSTRAINT `fk_copie_biblioteche` FOREIGN KEY (`id_biblioteca`) REFERENCES `biblioteche` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=10042 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10004 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -668,4 +668,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-01-25  2:00:03
+-- Dump completed on 2026-01-26  2:00:03
